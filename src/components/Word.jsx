a5bde011216e9f3cc486a787897855ec8a2413a9
jgrissom <jgrissom91@gmail.com>
2024-09-18 21:47:35 -0500

    [1.05] Css style attribute (jsx)

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -2,7 +2,19 @@ import { useState } from "react";
 
 function Word() {
   const [text, setText] = useState("banana");
-  return <div>{text}</div>;
+  const [color, setColor] = useState("yellow");
+  return (
+    <div
+      style={{
+        color: "var(--main-color)",
+        width: "fit-content",
+        fontSize: "3.5rem",
+        textShadow: `0px 0px 4px ${color}`,
+      }}
+    >
+      {text}
+    </div>
+  );
 }
 
 export default Word;
