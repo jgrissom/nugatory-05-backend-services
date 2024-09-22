d1d3a33939d02a5653677a1c313cda5e4c685ba9
jgrissom <jgrissom91@gmail.com>
2024-09-22 14:40:34 -0500

    [1.08] Handling events with jsx

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -3,8 +3,17 @@ import { useState } from "react";
 function Word() {
   const [text, setText] = useState("banana");
   const [color, setColor] = useState("yellow");
+
+  function handleClick() {
+    console.log(text);
+  }
+
   return (
-    <div className="word" style={{ textShadow: `0 0 .25rem ${color}` }}>
+    <div
+      className="word"
+      style={{ textShadow: `0 0 .25rem ${color}` }}
+      onClick={handleClick}
+    >
       {text}
     </div>
   );
