95741208c7d906a925e626f5d6ab7d55074d88cf
jgrissom <jgrissom91@gmail.com>
2024-09-22 14:35:01 -0500

    [1.06a] Create App component 🚀 🐧 Word component is a child of the App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import App from './App.jsx'
-import Word from "./components/Word";
+import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Word />
+    <App />
   </StrictMode>
 );
