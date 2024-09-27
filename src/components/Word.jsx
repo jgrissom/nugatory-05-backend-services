143fdb009cbc06c6f2158997d54dfff399ce252a
jgrissom <jgrissom91@gmail.com>
2024-09-27 18:39:16 -0500

    [3.04] Set top / left style attributes as each word is rendered

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,11 +1,14 @@
 import { ri } from "../Utils.js";
 
 export default function Word(props) {
-  console.log(ri(0, window.innerHeight));
   return (
     <div
       className="word"
-      style={{ textShadow: `0 0 .25rem ${props.word.color}` }}
+      style={{
+        textShadow: `0 0 .25rem ${props.word.color}`,
+        top: `${ri(0, window.innerHeight)}px`,
+        left: `${ri(0, window.innerWidth)}px`,
+      }}
       onClick={() => props.onDelete(props.word.id)}
     >
       {props.word.text}
