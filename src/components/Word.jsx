ec35cba8165c7d117994aee58bb33802d5f245db
jgrissom <jgrissom91@gmail.com>
2024-09-27 18:40:08 -0500

    [3.05] Create "ref" to DOM elements rendered by the Word component

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,8 +1,12 @@
+import { useRef } from "react";
 import { ri } from "../Utils.js";
 
 export default function Word(props) {
+  const wordRef = useRef();
+
   return (
     <div
+      ref={wordRef}
       className="word"
       style={{
         textShadow: `0 0 .25rem ${props.word.color}`,
