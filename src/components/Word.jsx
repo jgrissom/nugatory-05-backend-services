6522fdf40ebc5768faf63d03ff6b8a4656478970
jgrissom <jgrissom91@gmail.com>
2024-09-27 15:37:54 -0500

    [2.05] Remove local state 🚀 🐧 Use shorthand notation to export component

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,24 +1,17 @@
-import { useState } from "react";
-
-function Word(props) {
-  const [text, setText] = useState("banana");
-  const [color, setColor] = useState("yellow");
-
+export default function Word(props) {
   function handleClick() {
     // when a component's state is altered, it is re-rendered asynchronously by react
-    setColor("#000");
+    // setColor("#000");
   }
 
   console.log(props);
   return (
     <div
       className="word"
-      style={{ textShadow: `0 0 .25rem ${color}` }}
+      style={{ textShadow: `0 0 .25rem ${props.color}` }}
       onClick={handleClick}
     >
-      {text}
+      {props.text}
     </div>
   );
 }
-
-export default Word;
