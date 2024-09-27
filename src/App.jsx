86862ee0429495e6a00f925108f99dac4f291d00
jgrissom <jgrissom91@gmail.com>
2024-09-27 15:32:09 -0500

    [2.02] Passing data to components - json array 🚀 🐧 Create Word components with the array.map function
    🚀🚀🚀 NOTE: unique key prop warning in browser 🚀🚀🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import Word from "./components/Word";
 import "./App.css";
 
 function App() {
+  const [words, setWords] = useState([
+    { id: 1, text: "banana", color: "yellow" },
+    { id: 2, text: "apple", color: "#ff1a00" },
+    { id: 3, text: "lime", color: "#00ff00" },
+  ]);
+
   return (
     <div>
       <header className="app-header">nugatory</header>
-      <Word />
-      <Word />
-      <Word />
+      {words.map((word) => (
+        <Word />
+      ))}
     </div>
   );
 }
-
 export default App;
