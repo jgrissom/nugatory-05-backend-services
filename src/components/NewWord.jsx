73894a794a9615a3d5e12dd3e42d8ecb55b3d88e
jgrissom <jgrissom91@gmail.com>
2024-09-28 00:30:10 -0500

    [4.12c] Uncomment the form 🚀 🐧 Replace the hr element with form

--- src/components/NewWord.jsx
+++ src/components/NewWord.jsx
@@ -16,84 +16,93 @@ export default function NewWord(props) {
 
   return (
     <>
-      {/* <form onSubmit={(e) => handleSubmit(e)}>
-        <div id="overlay"></div>
-        <div id="dialog">
-          <header
-            style={{ textShadow: `0 0 0.25rem rgb(${red}, ${green}, ${blue})` }}
-          >
-            {text}
-          </header>
-          <div className="color">
-            <div
-              className="color-square"
-              style={{ background: `rgb(${red}, 0, 0)` }}
-            ></div>
-            <input
-              id="red"
-              type="range"
-              min="0"
-              max="255"
-              value={red}
-              onChange={(e) => setRed(e.target.value)}
-            />
-            <div className="value">{red}</div>
+      {showDialog ? (
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div id="overlay"></div>
+          <div id="dialog">
+            <header
+              style={{
+                textShadow: `0 0 0.25rem rgb(${red}, ${green}, ${blue})`,
+              }}
+            >
+              {text}
+            </header>
+            <div className="color">
+              <div
+                className="color-square"
+                style={{ background: `rgb(${red}, 0, 0)` }}
+              ></div>
+              <input
+                id="red"
+                type="range"
+                min="0"
+                max="255"
+                value={red}
+                onChange={(e) => setRed(e.target.value)}
+              />
+              <div className="value">{red}</div>
+            </div>
+            <div className="color">
+              <div
+                className="color-square"
+                style={{ background: `rgb(0, ${green}, 0)` }}
+              ></div>
+              <input
+                id="green"
+                type="range"
+                min="0"
+                max="255"
+                value={green}
+                onChange={(e) => setGreen(e.target.value)}
+              />
+              <div className="value">{green}</div>
+            </div>
+            <div className="color">
+              <div
+                className="color-square"
+                style={{ background: `rgb(0, 0, ${blue})` }}
+              ></div>
+              <input
+                id="blue"
+                type="range"
+                min="0"
+                max="255"
+                value={blue}
+                onChange={(e) => setBlue(e.target.value)}
+              />
+              <div className="value">{blue}</div>
+            </div>
+            <div>
+              <input
+                id="text"
+                type="text"
+                placeholder="new word"
+                maxLength="16"
+                autoCapitalize="off"
+                autoComplete="off"
+                autoCorrect="off"
+                autoFocus
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              />
+            </div>
+            <div>
+              <button
+                disabled={text.trim().length === 0}
+                type="submit"
+                id="save"
+              >
+                save
+              </button>
+            </div>
+            <div>
+              <button id="cancel">cancel</button>
+            </div>
           </div>
-          <div className="color">
-            <div
-              className="color-square"
-              style={{ background: `rgb(0, ${green}, 0)` }}
-            ></div>
-            <input
-              id="green"
-              type="range"
-              min="0"
-              max="255"
-              value={green}
-              onChange={(e) => setGreen(e.target.value)}
-            />
-            <div className="value">{green}</div>
-          </div>
-          <div className="color">
-            <div
-              className="color-square"
-              style={{ background: `rgb(0, 0, ${blue})` }}
-            ></div>
-            <input
-              id="blue"
-              type="range"
-              min="0"
-              max="255"
-              value={blue}
-              onChange={(e) => setBlue(e.target.value)}
-            />
-            <div className="value">{blue}</div>
-          </div>
-          <div>
-            <input
-              id="text"
-              type="text"
-              placeholder="new word"
-              maxLength="16"
-              autoCapitalize="off"
-              autoComplete="off"
-              autoCorrect="off"
-              autoFocus
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-            />
-          </div>
-          <div>
-            <button disabled={text.trim().length === 0} type="submit" id="save">
-              save
-            </button>
-          </div>
-          <div>
-            <button id="cancel">cancel</button>
-          </div>
-        </div>
-      </form> */}
-      {showDialog ? <hr /> : <img src={trigger} alt="New Word" id="trigger" />}
+        </form>
+      ) : (
+        <img src={trigger} alt="New Word" id="trigger" />
+      )}
     </>
   );
 }
